a0e804d989fbf3006ede001bb9c4d5691672463c
Kevin Sloan <61387934+KJSloan2@users.noreply.github.com>
2023-12-22 18:13:37 -0600

    Delete src/test.py

--- src/test.py
+++
@@ -1,8 +0,0 @@
-import csv
-naicsRef_ = {}
-with open("%s%s" % (r"00_resources/","naicsCodes.csv"), 'r', newline='') as csvfile:
-    csv_reader = csv.reader(csvfile)
-    for row in csv_reader:
-        naicsRef_[str(row[0])] = str(row[1])
-
-print(naicsRef_)
